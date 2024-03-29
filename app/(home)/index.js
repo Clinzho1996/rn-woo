7103bf59a0f7ee9b799ea2cf548bd1e5c9fbc937
Confidence Emonena <confidinho@yahoo.com>
2024-03-29 04:59:19 +0100

    first commit

---
+++ app/(home)/index.js
@@ -0,0 +1,134 @@
+import {
+  StyleSheet,
+  Text,
+  View,
+  Alert,
+  ScrollView,
+  Pressable,
+  TextInput,
+} from "react-native";
+import React, { useEffect, useState } from "react";
+import * as Location from "expo-location";
+import * as LocationGeocoding from "expo-location";
+import { Octicons } from "@expo/vector-icons";
+import { AntDesign } from "@expo/vector-icons";
+
+const index = () => {
+  const [locationServicesEnabled, setLocationServicesEnabled] = useState(false);
+  const [displayCurrentAddress, setDisplayCurrentAddress] = useState(
+    "fetching your location..."
+  );
+
+  useEffect(() => {
+    CheckIfLocationEnabled();
+    GetCurrentLocation();
+  }, []);
+
+  const CheckIfLocationEnabled = async () => {
+    let enabled = await Location.hasServicesEnabledAsync();
+
+    if (!enabled) {
+      Alert.alert(
+        "Location Services not enabled",
+        "Please enable your location services to continue",
+        [{ text: "OK" }],
+        { cancelable: false }
+      );
+    } else {
+      setLocationServicesEnabled(true);
+    }
+  };
+
+  const GetCurrentLocation = async () => {
+    let { status } = await Location.requestBackgroundPermissionsAsync();
+
+    if (status !== "granted") {
+      Alert.alert(
+        "Permission not granted",
+        "Allow the app to use the location service",
+        [{ text: "OK" }],
+        { cancelable: false }
+      );
+    }
+
+    const location = await Location.getCurrentPositionAsync({
+      accuracy: Location.Accuracy.High,
+    });
+    console.log(location);
+    let { coords } = await Location.getCurrentPositionAsync();
+    if (coords) {
+      const { latitude, longitude } = coords;
+
+      let response = await Location.reverseGeocodeAsync({
+        latitude,
+        longitude,
+      });
+
+      const address = await LocationGeocoding.reverseGeocodeAsync({
+        latitude,
+        longitude,
+      });
+
+      const streetAddress = address[0].name;
+      for (let item of response) {
+        let address = `${item.name}, ${item?.postalCode}, ${item?.city}`;
+
+        setDisplayCurrentAddress(address);
+      }
+    }
+  };
+  console.log("my address", displayCurrentAddress);
+  return (
+    <ScrollView style={{ flex: 1, backgroundColor: "#f8f8f8" }}>
+      <View
+        style={{
+          flexDirection: "row",
+          alignItems: "center",
+          gap: 12,
+          padding: 10,
+        }}
+      >
+        <Octicons name="location" size={24} color="#e52850" />
+        <View style={{ flex: 1 }}>
+          <Text style={{ fontSize: 15, fontWeight: "500" }}>Deliver To</Text>
+          <Text style={{ color: "gray", fontSize: 16, marginTop: 3 }}>
+            {displayCurrentAddress}
+          </Text>
+        </View>
+        <Pressable
+          style={{
+            backgroundColor: "#6cb4ee",
+            width: 40,
+            height: 40,
+            borderRadius: 20,
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Text>D</Text>
+        </Pressable>
+      </View>
+      <View
+        style={{
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "space-between",
+          borderWidth: 1,
+          borderColor: "#c0c0c0",
+          paddingVertical: 8,
+          paddingHorizontal: 10,
+          borderRadius: 11,
+          marginTop: 10,
+          marginHorizontal: 10,
+        }}
+      >
+        <TextInput placeholder="Search for food, hotels ..." />
+        <AntDesign name="search1" size={24} color="#e52b50" />
+      </View>
+    </ScrollView>
+  );
+};
+
+export default index;
+
+const styles = StyleSheet.create({});
